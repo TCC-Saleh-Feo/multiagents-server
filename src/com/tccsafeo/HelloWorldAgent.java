8a0d0088850d003bd54997512210b06f8bec0f15
devsalula <saleh.nazih.dev@gmail.com>
2022-04-05 22:51:14 -0300

    SetUp Project

---
+++ src/com/tccsafeo/HelloWorldAgent.java
@@ -0,0 +1,12 @@
+package com.tccsafeo;
+
+import jade.core.Agent;
+
+public class HelloWorldAgent extends Agent {
+    protected void setup() {
+        System.out.println("Hello world! I'm an agent!");
+        System.out.println("My local name is " + getAID().getLocalName());
+        System.out.println("My GUID is " + getAID().getName());
+        System.out.println("My addresses are " + String.join(",", getAID().getAddressesArray()));
+    }
+}
