95a51c08c6676d405be77f155d48138c7338eec9
phe0 <pheofo@gmail.com>
2022-08-23 18:55:47 -0300

    Add Calculator util

---
+++ src/com/tccsafeo/utils/Calculator.java
@@ -0,0 +1,32 @@
+package com.tccsafeo.utils;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    public static Double getSum(ArrayList<Double> values) {
+        Double sum = 0D;
+        for (Integer i = 0; i < values.size(); i++) {
+            sum += values.get(i);
+        }
+        return sum;
+    }
+
+    public static Double getAverage(ArrayList<Double> values) {
+        try {
+            return getSum(values) / values.size();
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException("A null value was passed as an argument!");
+        }
+    }
+
+    public static Double getStandardDeviation(ArrayList<Double> values) {
+        Double avg = getAverage(values);
+        Integer size = values.size();
+        Double standardDeviation = 0D;
+        for (Double value : values) {
+            Double aux = value - avg;
+            standardDeviation += aux * aux;
+        }
+        return Math.sqrt(standardDeviation / (size - 1));
+    }
+}
